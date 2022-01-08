0913ccc1b26b22c47e2590316db5aba4da9c9bd8
Bumtarata <svickovak@gmail.com>
2022-01-08 19:29:10 +0100

    Added high score.
    High score resets to zero after quiting instance of the game.

--- game_stats.py
+++ game_stats.py
@@ -5,9 +5,13 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
+        
         # Start Alien Invasion in an inactive state.
         self.game_active = False
         self.difficulty_chosen = False
+        
+        # High scores should never be reset.
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
